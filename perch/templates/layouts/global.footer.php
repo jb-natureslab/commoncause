60261b61f4168ccd8cab93f591549a60c5fd2a8a
Jack Barber <jack@natureslaboratory.co.uk>
2022-12-08 20:18:55 +0000

    Initial commit

---
+++ perch/templates/layouts/global.footer.php
@@ -0,0 +1,24 @@
+
+	</main>
+	
+	<footer>
+		<div>
+			<p class="size_400">
+				<strong>Three's Company &mdash; <span>Building Healthier Organisations</span></strong><br />
+				&copy; Nature's Laboratory Limited <?php echo date('Y'); ?><br />
+				Company Reg. 04375564 | <a href="/privacy/">Privacy</a> | <span>+44(0)1947 602346</span>
+			</p>
+			<ul>
+				<li><a href="https://ecologi.com/natureslaboratory?r=61af6aa4f9550a84cf8be3d8" target="_blank" rel="noopener noreferrer" title="View our Ecologi profile"><img alt="We offset our carbon footprint via Ecologi" src="https://api.ecologi.com/badges/cpw/61af6aa4f9550a84cf8be3d8?black=true&landscape=true" /></a></li>
+				<li><img src="/assets/images/living-wage.png" alt="Living Wage Employer" /></li>
+			</ul>
+		</div>
+	</footer>
+	</body>
+	<script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+	<script>
+		$('header nav button').click(function(){
+			$('header nav').toggleClass('show');
+		})
+	</script>
+	</html>
